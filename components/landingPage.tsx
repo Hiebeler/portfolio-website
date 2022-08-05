92cc3a4244c1f6f0886a3595972954651bd84299
Hiebeler <emanuel.hiebeler@gmail.com>
2022-08-05 23:58:01 +0200

    updated responsive design

--- components/landingPage.tsx
+++ components/landingPage.tsx
@@ -5,15 +5,22 @@ import Wave from 'react-wavify'
 function LandingPage() {
     return <div className='h-screen bg-light_background'>
         <div className='h-5/6 relative'>
-            <div className='flex flex-row flex-wrap h-full p-20'>
-                <div className='sm:shrink w-full md:basis-1/2 items-center justify-center relative'>
-                    <Image src="/images/landingPageImage.png" alt="software Engineer Illustration" layout='fill' objectFit='contain' />
+            <div className='flex flex-row flex-wrap h-full sm:p-20 p-10'>
+                <div className='w-full md:basis-1/2 items-center justify-center relative'>
+                    <div className='hidden md:block ' >
+                        <Image src="/images/landingPageImage.png" alt="software Engineer Illustration" layout='fill' objectFit='contain' />
+                    </div>
+                    <div className='block md:hidden items-center justify-center' >
+                        <Image src="/images/avatar.png" alt="Avatar" layout='fill' objectFit='contain' className='hidden md:block' />
+                    </div>
                 </div>
                 <div className='sm:flex-1 md:basis-1/2 flex items-center'>
                     <div className='flex-col'>
                         <div className='flex flex-row items-center relative'>
                             <h1 className='text-primary text-8xl font-dosis basis-2/5'>Hello</h1>
-                            <Image src="/images/avatar.png" alt="Avatar" height="150px" width="150px" />
+                            <div className='hidden md:block'>
+                                <Image src="/images/avatar.png" alt="Avatar" height="130px" width="130px" className='hidden md:block relative' />
+                            </div>
                         </div>
                         <p className='font-dosis text-xl text-secondary max-w-xl'>I'm Emanuel Hiebeler, a programmer interested in mobile, web and backend development. </p>
                     </div>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,16 @@ import '../styles/globals.css'
 import 'tailwindcss/tailwind.css'
 import LandingPage from '../components/landingPage'
 import AboutMe from '../components/aboutMe'
+import Head from 'next/head'
 
 function MyApp() {
   return <div>
-   <LandingPage/>
-   <AboutMe />
+    <Head>
+      <title>Next App</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+    <LandingPage />
+    <AboutMe />
   </div>
 }
 
