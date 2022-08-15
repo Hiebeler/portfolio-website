3e263bf68f5f092a9ab4701a8ec25d18b1b4b0af
Hiebeler <emanuel.hiebeler@gmail.com>
2022-08-15 22:26:43 +0200

    horizontal scroll(no smooth scrolling yet)

--- components/project-card.tsx
+++ components/project-card.tsx
@@ -1,8 +1,12 @@
 import 'tailwindcss/tailwind.css'
 
-function projectCard() {
-    return <div className="bg-primary">
-        hallo
+function projectCard(props: any) {
+    const title: String = props["title"];
+    const color = props["color"];
+    return <div className="w-full h-[70vh] px-10">
+        <div className='bg-primary h-full'>
+            {title}
+        </div>
     </div>
 }
 
