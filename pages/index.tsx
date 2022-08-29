f2266868298f0229177f357767b8c19e633a6800
Hiebeler <emanuel.hiebeler@gmail.com>
2022-08-29 19:04:12 +0200

    avatar image quadratisch

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,7 @@ function Home() {
         <Head>
             <title>Emanuel Hiebeler</title>
             <link rel="icon" href="/favicon.ico" />
-            <meta name="description" content="I am Emanue Hiebeler, a sailor and programmer. I am interested in programming especially in web and app development." />
+            <meta name="description" content="I am Emanue Hiebeler, a sailor and programmer. I'm especially interested in web and app development." />
         </Head>
         <LandingPage />
         <AboutMe />
