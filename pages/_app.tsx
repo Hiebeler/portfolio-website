201ee74b5614c748c103af57c2f1c4d71834b598
Hiebeler <emanuel.hiebeler@gmail.com>
2022-08-29 18:47:30 +0200

    fixed 404 error

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,23 +1,8 @@
 import '../styles/globals.css'
-import 'tailwindcss/tailwind.css'
-import LandingPage from '../components/landingPage'
-import AboutMe from '../components/aboutMe'
-import Head from 'next/head'
-import Projects from '../components/projects'
+import type { AppProps } from 'next/app'
 
-function MyApp() {
-  return <div className='overflow-x-hidden'>
-    <Head>
-      <title>Emanuel Hiebeler</title>
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
-    <LandingPage />
-    <AboutMe />
-    <Projects />
-    <div className='h-screen bg-dark_background relative'>
-      <h2 className='text-primary text-6xl font-dosis'>Contact Me:</h2>
-    </div>
-  </div>
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
 }
 
 export default MyApp
---
+++ pages/index.tsx
@@ -0,0 +1,23 @@
+import 'tailwindcss/tailwind.css'
+import LandingPage from '../components/landingPage'
+import AboutMe from '../components/aboutMe'
+import Head from 'next/head'
+import Projects from '../components/projects'
+
+function Home() {
+
+    return <div className='overflow-x-hidden'>
+        <Head>
+            <title>Emanuel Hiebeler</title>
+            <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <LandingPage />
+        <AboutMe />
+        <Projects />
+        <div className='h-screen bg-dark_background relative'>
+            <h2 className='text-primary text-6xl font-dosis'>Contact Me:</h2>
+        </div>
+    </div>
+}
+
+export default Home
