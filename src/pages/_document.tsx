293a1c48c2a4942781d9dde13b85781fceb56eea
Emanuel Hiebeler <78096107+Hiebeler@users.noreply.github.com>
2026-02-05 13:51:40 +0100

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,9 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <link rel="me" href="https://techhub.social/@Hiebeler05" />
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
