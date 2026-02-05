5ee98fda7e289607a2e8faaa52617527af507244
Emanuel Hiebeler <78096107+Hiebeler@users.noreply.github.com>
2026-02-05 13:40:58 +0100

    add mastodon verification link

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="me" href="https://techhub.social/@Hiebeler05" />
+      </Head>
       <body>
         <Main />
         <NextScript />
